4913ad7d3129366fab36685185a5a642ca86b76a
adrian <35755741+adrian-kong@users.noreply.github.com>
2018-10-28 19:45:33 +1100

    Add files via upload

---
+++ lissajous/script.js
@@ -0,0 +1,64 @@
+var canvas  = document.querySelector('canvas'),
+    context = canvas.getContext('2d'),
+    width   = canvas.width,
+    height  = canvas.height,
+    angle   = 0,
+    r       = 50,
+    ampX    = 1,
+    ampY    = 1,
+    phaseX  = 0,
+    phaseY  = 0,
+    periodX = 1,
+    periodY = 1,
+    running = true,
+    x,y;
+
+context.beginPath();
+render();
+
+function render() {
+  if (!running) {
+    return;
+  }
+  x = width / 2  + ampX * Math.cos(periodX * angle - phaseX) * r;
+  y = height / 2 + ampY * Math.cos(periodY * angle - phaseY) * r;
+  context.lineTo(x,y);
+  context.stroke();
+  angle += 0.01;
+  requestAnimationFrame(render);
+};
+
+function toggle(){
+  running = !running;
+  render();
+};
+
+function dampX() {
+  ampX = document.getElementById('amplitudeX').value;
+};
+
+function dampY() {
+  ampY = document.getElementById('amplitudeY').value;
+};
+
+function dphaseX() {
+  phaseX = document.getElementById('phaseX').value;
+};
+
+function dperiodX() {
+  periodX = document.getElementById('periodX').value;
+};
+
+function dphaseY() {
+  phaseY = document.getElementById('phaseY').value;
+};
+
+function dperiodY() {
+  periodY = document.getElementById('periodY').value;
+};
+
+function reset(){
+  context.clearRect(0,0,800,800);
+  angle = 0;
+  context.beginPath();
+};
